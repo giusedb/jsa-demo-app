44a2bb20ecabe0055f851ca05cc06f375e89daae
Giuseppe Di Bona <giusedb@gmail.com>
2025-11-23 23:24:02 +0100

    introduce test cmoponent

---
+++ client/app/pages/Test.vue
@@ -0,0 +1,21 @@
+<script lang="ts">
+import {defineComponent} from 'vue'
+
+export default defineComponent({
+  name: "Test"
+})
+</script>
+
+<template>
+  <div>
+    <h2>Test playground</h2>
+    <p>
+      Use this page for testing components and features you need to make before making them in
+      an effective component.
+    </p>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
