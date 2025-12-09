bbb8c9dc0de19d57f335dd933e21a38007a73eb4
Giuseppe Di Bona <giusedb@gmail.com>
2025-12-10 00:27:23 +0100

    Add realtime server capabilities

--- server/cli/web.py
+++ server/cli/web.py
@@ -1,5 +1,6 @@
 import click
 
+from config import get_config
 from .base import main
 
 @main.group()
@@ -7,7 +8,6 @@ def web():
     """Web section"""
 
 def parse_web_defaults(cli: dict):
-    from config import get_config
     config = get_config()
     web_config = config.get('web', {})
     web_default = {
@@ -39,3 +39,27 @@ def blacksheep(host, port, debug):
     cli = parse_web_defaults(dict(host=host, port=port, debug=debug))
     cli['reload'] = cli.pop('debug')
     uvicorn.run('web.blacksheep:app', **cli)
+
+@web.command()
+@click.option('-h', '--host', default=None)
+@click.option('-p', '--port', default=None)
+@click.option('-c', '--redis-channel', default=None)
+def realtime(host, port, redis_channel):
+    """Run realtime server"""
+    from jsalchemy_api.realtime import WSServer
+
+    config = get_config().get('web')
+    rt_config = config.get('realtime')
+    if rt_config is False:
+        raise click.ClickException("Realtime server is explitely disabled py configuration")
+
+    print(f"🚀 Starting realtime server on {rt_config['host']}:{rt_config['port']}")
+
+    server = WSServer(
+        host=rt_config['host'],
+        port=rt_config['port'],
+        redis_channel=rt_config['redis_channel'],
+        redis_url=config.get('session', {}).get('connection'),
+    )
+
+    server.start()
