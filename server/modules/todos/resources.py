6dad6bce7bb48d7074aeecf12fa8aaa6a58363d5
Giuseppe Di Bona <giusedb@gmail.com>
2026-03-20 17:06:49 +0100

    Add todo `deleteAll()`

--- server/modules/todos/resources.py
+++ server/modules/todos/resources.py
@@ -1,4 +1,8 @@
+from sqlalchemy import delete, select
+
 from jsalchemy_api import DBResource, ResourceManager
+from jsalchemy_api.resources.base import verb
+from jsalchemy_web_context import db
 from modules.todos.models import Todo
 
 
@@ -10,3 +14,7 @@ class TodoResource(DBResource):
             resource_manager=resource_manager,
             rpp=100,
         )
+
+    @verb(detached_instance=True)
+    async def delete_all(self):
+        await db.execute(delete(self.model).where(self.model.id > 0))
